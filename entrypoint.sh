9767f5152a6abc19e5c36b861d1d80cf233187b0
Tim Welch <tim.j.welch@gmail.com>
2023-03-08 16:42:52 -0800

    flag tmp dir as safe for git repo to live

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,6 +44,7 @@ debug "Checking out wiki repository"
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 (
     cd "$tmp_dir" || exit 1
+    git config --global --add safe.directory /tmp
     git init
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
